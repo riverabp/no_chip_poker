0b01d1d1f9f7e7af5d890784e969d7e67209895a
Ben Rivera <benajmin.p.rivera@vanderbilt.edu>
2017-01-24 22:08:07 -0600

    dealer comments

--- src/com/company/Dealer.java
+++ src/com/company/Dealer.java
@@ -1,28 +1,40 @@
 package com.company;
 
 /**
- * Created by Ben on 12/27/16.
+ * A Dealer has a deck of cards and draws cards to place on the board.
  */
 public class Dealer {
 
     public static final double PLAYERS = 2;
     public static final int BOARD_SIZE = 5;
+    private static final int BURNED_CARDS = 3;
 
     private Card[] board;
+    private Card[] burnedCards;
     private int boardSize;
     private Deck deck;
 
-    //create board and empty pot
+    /**
+     * Default ctor.
+     */
     Dealer(){
         boardSize = 0;
         board = new Card[BOARD_SIZE];
+        burnedCards = new Card[BURNED_CARDS];
         deck = new Deck();
     }
 
+    /**
+     * @return Card drawn card
+     */
     public Card dealCard(){
         return deck.draw();
     }
 
+    /**
+     * deals two hole cards to each player
+     * @param table list of players
+     */
     public void preFlop(Table table){
         for (int i = 0; i < table.getPlayerCount(); i++){
             Card c1 = deck.draw();
@@ -31,24 +43,38 @@ public class Dealer {
         }
     }
 
-    //flop 3 cards
+    /**
+     * Burn one card and place three on the board
+     */
     public void flop(){
+        burnedCards[0] = new Card();
         board[0] = new Card();
         board[1] = new Card();
         board[2] = new Card();
         boardSize = 3;
     }
 
+    /**
+     * Burn one card and place the turn on the board
+     */
     public void turn(){
+        burnedCards[1] = new Card();
         board[3] = new Card();
         boardSize = 4;
     }
 
+    /**
+     * Burn one card and place the river on the board
+     */
     public void river(){
+        burnedCards[2] = new Card();
         board[4] = new Card();
         boardSize = 5;
     }
 
+    /**
+     * Print the board cards to the console
+     */
     public void printBoard(){
         System.out.print("Board Cards: ");
         for(int i = 0; i < boardSize; i++){
